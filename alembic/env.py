9f10680a2cd4d6b3bfd79c43c239c85db620eee8
Apolo Rae <aresjae777@gmail.com>
2025-07-06 19:30:33 -0400

    wip: run migrations offline

--- alembic/env.py
+++ alembic/env.py
@@ -17,19 +17,6 @@ target_metadata = Base.metadata
 config.set_main_option("sqlalchemy.url", settings.database_url)
 
 
-def run_migrations_offline() -> None:
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-        compare_type=True,
-    )
-    with context.begin_transaction():
-        context.run_migrations()
-
-
 def run_migrations_online() -> None:
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),

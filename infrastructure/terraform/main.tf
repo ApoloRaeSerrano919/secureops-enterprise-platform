02def061d3d6cde3cfa8d3f5fb0505e4ba423d41
Apolo Rae <aresjae777@gmail.com>
2026-03-23 19:55:42 -0400

    ci: main pass

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -22,3 +22,8 @@ resource "aws_ecr_repository" "event_gateway" {
   image_scanning_configuration { scan_on_push = true }
 }
 
+resource "aws_cloudwatch_log_group" "api" {
+  name              = "/${var.project_name}/api"
+  retention_in_days = 30
+}
+

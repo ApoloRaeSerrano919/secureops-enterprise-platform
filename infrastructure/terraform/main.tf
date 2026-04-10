1afd6676dd878ed5b94ccc0b988bb15947dae056
Apolo Rae <aresjae777@gmail.com>
2026-04-10 15:18:44 -0400

    wip: main

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -13,6 +13,14 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_ecr_repository" "api" {
+  name = "${var.project_name}-api"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 resource "aws_cloudwatch_log_group" "api" {
   name              = "/${var.project_name}/api"
   retention_in_days = 30
